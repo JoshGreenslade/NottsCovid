47546d328061f55755896dbf32da7fd066845434
Josh Greenslade <joshua.greenslade@onyxinsight.com>
2020-12-19 12:42:59 +0000

    Added UI Ctrl

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import json
 import dash
-from src import dataCtrl
+from src import dataCtrl, uiCtrl
 
 # Load in configuration
 with open('./config/nottsConfig.json', 'r') as f:
     config = json.load(f)
 
-data_ctrl = dataCtrl.DataCtrl()
-print(data_ctrl.get_data_for_area('E02006905'))
-
 # Init the app
 app = dash.Dash(__name__)
+
+data_ctrl = dataCtrl.DataCtrl()
+
+ui_ctrl = uiCtrl.UiCtrl(data_ctrl, app)
+
 
 # Run the app
 if __name__ == '__main__':
